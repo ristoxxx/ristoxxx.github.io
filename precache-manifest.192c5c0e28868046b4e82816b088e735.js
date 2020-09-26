ee71b490a909949a638284c347f050bbf9b554ee
ristoxxx <lahteri@gmail.com>
2020-09-26 18:37:22 +0300

    testi

---
+++ precache-manifest.192c5c0e28868046b4e82816b088e735.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97332b7813cef081049598487fae2741",
+    "url": "/git@github.com:ristoxxx/ristoxxx.github.io.git/index.html"
+  },
+  {
+    "revision": "b1f8288e52c0756ee2ae",
+    "url": "/git@github.com:ristoxxx/ristoxxx.github.io.git/static/js/2.1e571c22.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/git@github.com:ristoxxx/ristoxxx.github.io.git/static/js/2.1e571c22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76ec4350a89fb5d0e46a",
+    "url": "/git@github.com:ristoxxx/ristoxxx.github.io.git/static/js/main.4327a8e1.chunk.js"
+  },
+  {
+    "revision": "968f729fb45757655fe8",
+    "url": "/git@github.com:ristoxxx/ristoxxx.github.io.git/static/js/runtime-main.6cdde51b.js"
+  }
+]);
