390bc8cec6c4b9603f3ad3d25307cfe6bca78fb7
ristoxxx <lahteri@gmail.com>
2024-01-16 01:43:03 +0200

    Create project.js

---
+++ modules/project.js
@@ -0,0 +1,34 @@
+const projectData = [
+    {
+        name: "Academic Work",
+        description: "Company wide system renewal project of corporation size of company.",
+        link: "www.academicwork.fi"
+    },
+    {
+        name: "Ensto",
+        description: "Development of an IoT system designed to generate alarms based on specific metrics. Throughout this project, I gained hands-on experience and familiarity with various systems and technologies, including: • LoraWan • NB-IoT • MQTT",
+        link: "www.ensto.fi"
+    },
+    {
+        name: "Pop-Up meetingrooms project",
+        description: "IoT-project where multinational team of 17 people produced IoT-system that shows current reservation status of pop-up meeting spaces • Full stack • Documenting • MQTT",
+        link: "https://github.com/Popup-Meeting-Rooms-Project"
+    },
+    {
+        name: "Escape room lock project",
+        description: "nnovation course coursework. IoT-project that reads barcodes and opens lock with correct barcode",
+        link: "https://github.com/ristoxxx/innohell"
+    },
+    {
+        name: "Virtual 'speed camera'",
+        description: "Microservice that recieves HSL bus beacon MQTT data stream and makes alarm if bus is overspeeding at pre-defined location.",
+        link: "https://github.com/ristoxxx/bussiMQTT"
+    },
+    {
+        name: "Personal trainer calendar",
+        description: "Basic calendar tool for making reservations.",
+        link: "https://github.com/ristoxxx/personal-trainer/tree/main"
+    }
+    ];
+
+export default projectData;
